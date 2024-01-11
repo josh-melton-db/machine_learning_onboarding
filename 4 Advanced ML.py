807c5c10b072c4206acad0987de5fa2167fb4c25
josh-melton-db <josh-melton-db>
2024-01-11 20:59:47 +0000

    updated model specific inference

--- 4 Advanced ML.py
+++ 4 Advanced ML.py
@@ -102,16 +102,18 @@ features_density.display()
 # DBTITLE 1,Add Arima Forecast
 from pyspark.sql.functions import pandas_udf, lit
 from statsmodels.tsa.arima.model import ARIMA
+import mlflow 
 
 @pandas_udf("double")
 def forecast_arima(temperature: pd.Series, order_series: pd.Series) -> pd.Series:
+    mlflow.sklearn.autolog(disable=True)
     order = tuple(map(int, order_series.iloc[0].strip('()').split(',')))
     model = ARIMA(temperature, order=order)
     model_fit = model.fit()
     return model_fit.predict()
 
 # Minimal Spark code - just select one column and add another. We can still use Pandas for our logic
-features_arima = features_density.select('temperature').withColumn('predicted_temp', forecast_arima('temperature', lit('(1, 2, 4)')))
+features_arima = features_density.withColumn('predicted_temp', forecast_arima('temperature', lit('(1, 2, 4)')))
 features_arima.display()
 
 # COMMAND ----------
@@ -132,7 +134,7 @@ rmse
 # COMMAND ----------
 
 # DBTITLE 1,Define Objective Function
-train_arima_features = (
+tune_arima_features = (
     spark.read.table(config['bronze_table'])
     .orderBy('timestamp')
     .select('temperature')
@@ -143,10 +145,9 @@ train_arima_features = (
 def objective(params):
     order = str((int(params['p']), int(params['d']), int(params['q'])))
     temp_predictions = (
-        train_arima_features.dropna()
+        tune_arima_features.dropna()
         .select('temperature')
         .withColumn('predicted_temp', forecast_arima('temperature', lit(order)))
-        .where('predicted_temp is not null and temperature is not null')
     )
     return evaluator.evaluate(temp_predictions)
 
@@ -157,7 +158,6 @@ print(objective({'p': 1, 'd': 1, 'q': 1}), objective({'p': 1, 'd': 2, 'q': 0}))
 
 # DBTITLE 1,Hyperparameter Tune ARIMA
 from hyperopt import fmin, tpe, hp, SparkTrials
-import mlflow 
 
 # Define search space. Many possibilities, but Hyperopt identifies the best combinations to try
 search_space = {'p': hp.quniform('p', 0, 3, 1),
@@ -176,15 +176,9 @@ print('Optimal hyperparameters: ', argmin)
 
 # COMMAND ----------
 
-# DBTITLE 1,Train New Model
+# DBTITLE 1,Train New Defect Model
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import recall_score
-
-# Use our optimal ARIMA hyperparameters and add the predictions as a feature
-optimal_order = str((int(argmin['p']), int(argmin['d']), int(argmin['q'])))
-arima_features = spark.read.table(config['silver_features'])
-arima_features = arima_features.withColumn('predicted_temp', forecast_arima('temperature', lit(optimal_order)))
-arima_features = arima_features.toPandas()
 
 def train_with_arima(pdf: pd.DataFrame):
     X_train = pdf.drop('defect', axis=1)
@@ -195,40 +189,30 @@ def train_with_arima(pdf: pd.DataFrame):
 
 mlflow.sklearn.autolog()
 with mlflow.start_run(run_name='With ARIMA Features') as run:
-    rf_model = train_with_arima(arima_features)
+    rf_model = train_with_arima(features_arima.where('device_id = 1').toPandas())
 mlflow.sklearn.autolog(disable=True)
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC # Custom MLflow Models
-# MAGIC Sometimes an out of the box model from one of the libraries MLflow integrates won't get the job done, so you need to do something custom. Adding last-second transformations to inputs or outputs of a model or combining the results of two different models as a single model might stop you from logging a model via the standard MLflow integrations. Luckily, there's a way to create custom models in MLflow by subclassing PythonModel and including a predict() function like below:
-# MAGIC </br></br>
-# MAGIC ```
-# MAGIC class Add5(mlflow.pyfunc.PythonModel):
-# MAGIC     def predict(self, context, model_input):
-# MAGIC         return 5 + model_input['feature_col'] 
-# MAGIC
-# MAGIC with mlflow.start_run() as run:
-# MAGIC     mlflow.pyfunc.log_model('model', python_model=Add5())
-# MAGIC ```
-# MAGIC
-# MAGIC What happens when we want to make an update to the ARIMA model? Or when we change our feature logic? If the defect model was trained with a different input, it may produce incorrect results. Let's use a custom MLflow model to make sure that our defect model always gets paired with the the intended ARIMA model and features
 
 # COMMAND ----------
 
 # DBTITLE 1,Define Custom Model
 class ComboModel(mlflow.pyfunc.PythonModel):
-    def __init__(self, defect_model, optimal_order):
+    def __init__(self, defect_model=None, order=(1, 1, 1), ohe_list=[]):
         self.defect_model = defect_model
-        self.order = optimal_order
+        self.order = order
+        self.ohe_list = ohe_list
     
-    def ohe_encoding(self, pdf):
+    def ohe_encoding(self, pdf: pd.DataFrame) -> pd.DataFrame:
         encoded_factory = pd.get_dummies(pdf['factory_id'], prefix='ohe')
         encoded_model = pd.get_dummies(pdf['model_id'], prefix='ohe')
         pdf = pd.concat([pdf.drop('factory_id', axis=1).drop('model_id', axis=1), encoded_factory, encoded_model], axis=1)
         pdf = pdf.drop('timestamp', axis=1)
-        return pdf 
+        return pdf
+
+    def add_missing_ohes(self, pdf: pd.DataFrame):
+        ohes_to_add = [column for column in self.ohe_list if column not in pdf.columns]
+        for col in ohes_to_add:
+            pdf[col] = 0
+        return pdf
 
     def add_rolling_temp(self, pdf: pd.DataFrame) -> pd.DataFrame:
         pdf['rolling_mean_temp'] = pdf['temperature'].ewm(5).mean()
@@ -241,40 +225,45 @@ class ComboModel(mlflow.pyfunc.PythonModel):
         pdf = pdf.fillna(method='ffill').fillna(0)
         return pdf
     
-    def forecast_arima(self, df: pd.DataFrame) -> pd.DataFrame:
-        model = ARIMA(df.temperature, order=self.order)
+    def forecast_arima(self, pdf: pd.DataFrame) -> pd.DataFrame:
+        model = ARIMA(pdf.temperature, order=self.order)
         model_fit = model.fit()
-        df['predicted_temp'] = model_fit.predict()
-        return df
+        pdf['predicted_temp'] = model_fit.predict()
+        return pdf
 
-    def generate_features(self, df):
-        features = self.ohe_encoding(df)
+    def generate_features(self, pdf: pd.DataFrame) -> pd.DataFrame:
+        features = self.ohe_encoding(pdf)
+        features = self.add_missing_ohes(features)
         features = self.add_rolling_temp(features)
         features = self.add_rolling_density(features)
         features = self.forecast_arima(features)
         return features
 
     def predict(self, context, model_input):
-        return pd.DataFrame(self.defect_model.predict(model_input))
-
-combo_model = ComboModel(rf_model, (int(argmin['p']), int(argmin['d']), int(argmin['q'])))
+        output = self.generate_features(model_input)
+        if self.defect_model:
+            output['prediction'] = self.defect_model.predict(output)
+        return output
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC By logging the feature logic along with our model we could eliminate a lot of potential headaches in productionalizing our model, such as online/offline skew. In this example, ensuring that our ARIMA model gets the appropriate optimal_order parameter means we can create features as our model expects them. In production this could even be coupled with some of the pandas parallelization techniques we saw earlier since we've defined a single featurization function that accepts and returns a pandas dataframe. For now, let's log this custom model to MLflow and use it to make predictions
+# MAGIC By logging the feature logic along with our model we could eliminate a lot of potential headaches in productionalizing our model, such as online/offline skew. In this example, ensuring that our ARIMA model gets the appropriate optimal_order parameter means we can create features as our model expects them. Let's log this custom model to MLflow and use it to generate features and make predictions - we can call the predict() function and it will run our code in parallel across our cluster as a Pandas UDF
 
 # COMMAND ----------
 
-# DBTITLE 1,Sample Model's Features
-raw = spark.read.table(config['bronze_table']).limit(5).toPandas().drop('defect', axis=1)
-combo_features = combo_model.generate_features(raw) # generate features with our model
+# DBTITLE 1,Test Feature Generation
+ohe_list = [column for column in features_arima.columns if 'ohe' in column]
+optimal_order = (int(argmin['p']), int(argmin['d']), int(argmin['q']))
+combo_model = ComboModel(None, optimal_order, ohe_list)
 
-with mlflow.start_run() as run:
-    mlflow.pyfunc.log_model('combo_model', python_model=combo_model, input_example=combo_features) 
+raw = spark.read.table(config['bronze_table']).limit(5).toPandas()
+
+with mlflow.start_run(run_name='') as run:
+    mlflow.pyfunc.log_model('combo_model', python_model=combo_model, input_example=raw) 
 
 custom_model = mlflow.pyfunc.load_model(f'runs:/{run.info.run_id}/combo_model')
-display(custom_model.predict(combo_features)) # test that loading our model works
+display(custom_model.predict(raw)) # test that our model works for feature generation
 
 # COMMAND ----------
 
@@ -291,32 +280,54 @@ display(custom_model.predict(combo_features)) # test that loading our model work
 
 # COMMAND ----------
 
+# DBTITLE 1,Nested
 def single_model_run(pdf: pd.DataFrame) -> pd.DataFrame:
     run_id = pdf["run_id"].iloc[0]
     model_id = pdf["model_id"].iloc[0]
     n_used = pdf.shape[0]
     with mlflow.start_run(run_id=run_id) as outer_run:  # Set the top level run
         experiment_id = outer_run.info.experiment_id    # and nest the inner runs
-        with mlflow.start_run(run_name=model_id, nested=True, experiment_id=experiment_id):
-            features = combo_model.generate_features(pdf.drop('run_id', axis=1))
+        with mlflow.start_run(run_name=model_id, nested=True, experiment_id=experiment_id) as inner_run:
+            features = custom_model.predict(pdf.drop('run_id', axis=1))
             rf_model = train_with_arima(features)
-            model_specific_ml = ComboModel(rf_model, optimal_order)
-            mlflow.pyfunc.log_model('combo_model', python_model=model_specific_ml, input_example=pdf.head()) 
-            return_df = pd.DataFrame([[model_id, n_used]], columns=["model_id", "n_used"])
+            model_specific_ml = ComboModel(rf_model, optimal_order, ohe_list)
+            mlflow.pyfunc.log_model(f'combo_model_{model_id}', python_model=model_specific_ml, input_example=features.head()) 
+            mlflow.set_tag('model_id', model_id)
+            return_df = pd.DataFrame([[model_id, inner_run.info.run_id, n_used]], columns=["model_id", "run_id", "n_used"])
     return return_df
 
-train_return_schema = "model_id string, n_used integer"
+train_return_schema = "model_id string, run_id string, n_used int"
 
-raw = spark.read.table(config['bronze_table'])
+bronze_df = spark.read.table(config['bronze_table'])
+
 with mlflow.start_run(run_name="Device Specific Models") as run:
-    run_id = run.info.run_id
     model_info = (
-        raw
-        .withColumn("run_id", lit(run_id)) # Add run_id
+        bronze_df
+        .withColumn("run_id", lit(run.info.run_id)) # Pass in run_id
         .groupby("model_id")
-        .applyInPandas(single_model_run, schema=train_return_schema)
+        .applyInPandas(single_model_run, train_return_schema)
     )
 model_info.display()
+
+# COMMAND ----------
+
+# DBTITLE 1,Model Specific Inference
+from mlflow.pyfunc import PythonModel
+
+class OriginDelegatingModel(PythonModel):
+    def __init__(self, model_map):
+        self.model_map = model_map
+    
+    def predict(self, model_input):
+        model_id = model_input.iloc[0]['model_id']
+        model = self.model_map.get(model_id)
+        return model.predict(model_input)
+
+model_map = {row['model_id']: mlflow.pyfunc.load_model(f'runs:/{row["run_id"]}/combo_model_{row["model_id"]}')
+             for row in model_info.collect()}
+
+inference_model = OriginDelegatingModel(model_map)
+inference_model.predict(bronze_df.drop('defect').sample(.01).toPandas())
 
 # COMMAND ----------
 
@@ -329,3 +340,5 @@ model_info.display()
 # MAGIC - hyperparameter tuning
 # MAGIC - custom MLflow models
 # MAGIC - nested MLflow runs
+# MAGIC
+# MAGIC The next notebook is a bonus for if you're interested in combining the distributed processing power of Databricks with popular deep learning frameworks
